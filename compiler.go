7707cd51dd88a9d5ec0c570431024849e45b5b30
Qi Xiao <xiaqqaix@gmail.com>
2021-10-12 00:23:11 +0100

    Consolidate files

--- compiler.go
+++ compiler.go
@@ -78,3 +78,18 @@ func (cp *compiler) popScope() {
 	cp.scopes[len(cp.scopes)-1] = nil
 	cp.scopes = cp.scopes[:len(cp.scopes)-1]
 }
+
+type staticNs map[string]struct{}
+
+func (ns staticNs) del(k string) {
+	delete(ns, k)
+}
+
+func (ns staticNs) add(k string) {
+	ns[k] = struct{}{}
+}
+
+func (ns staticNs) has(k string) bool {
+	_, ok := ns[k]
+	return ok
+}
