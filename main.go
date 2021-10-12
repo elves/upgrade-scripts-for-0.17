4878bacbd8533d3a6e4cedd1f3165f99de4fee54
Qi Xiao <xiaqqaix@gmail.com>
2021-10-12 22:53:35 +0100

    Support processing files

--- main.go
+++ main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
-	"log"
 	"os"
 
 	"github.com/elves/upgrade-scripts-for-0.17/fix"
+	"src.elv.sh/pkg/diag"
 	"src.elv.sh/pkg/parse"
 )
 
+var rewrite = flag.Bool("r", false, "rewrite files")
+
 func main() {
-	code, err := io.ReadAll(os.Stdin)
+	flag.Parse()
+	args := flag.Args()
+	if len(args) == 0 {
+		process(os.Stdin, os.Stdout)
+	} else {
+		for _, arg := range args {
+			f, err := os.OpenFile(arg, os.O_RDWR, 0)
+			if err != nil {
+				diag.ShowError(os.Stderr, err)
+				continue
+			}
+			w := os.Stdout
+			if *rewrite {
+				w = f
+			}
+			process(f, w)
+		}
+	}
+}
+
+func process(r io.Reader, w io.Writer) {
+	code, err := io.ReadAll(r)
 	if err != nil {
-		log.Fatal(err)
+		diag.ShowError(os.Stderr, err)
+		return
 	}
 	fixed, err := fix.Fix(parse.Source{Name: "[stdin]", Code: string(code)})
 	if err != nil {
-		log.Fatal(err)
+		diag.ShowError(os.Stderr, err)
+		return
 	}
-	fmt.Print(fixed)
-	// TODO: Support rewriting files
+	if s, ok := w.(io.Seeker); ok {
+		s.Seek(0, io.SeekStart)
+	}
+	fmt.Fprint(w, fixed)
 }
